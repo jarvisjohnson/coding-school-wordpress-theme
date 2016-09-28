c7fdd4d9608a34d1afc47a2689fef4c14c0bcb0d
Jarvis Johnson <jarviscjohnson@gmail.com>
2016-09-28 16:27:00 -0400

    switcher pretty much finished

--- template-parts/switcher.php
+++ template-parts/switcher.php
@@ -1,22 +1,26 @@
 <script type="text/javascript">
 $( "#online-courses").click(function() {
   $( this ).addClass( "active" ).siblings().removeClass('active');
+  $('.online').addClass( "active" );
+  $('.offline').removeClass( "active" );
   if (!$( this ).hasClass( "active" )) {
-  	$('.product_cat-on-campus').addClassremoveClass( "display" );  	
-  	$('.product_cat-online').addClass( "no-display" );
+  	$('.product_cat-on-campus').addClass( "display" );  	
+  	$('.product_cat-online').addClass( "no-display" );	
   } else {
   	$('.product_cat-on-campus').removeClass( "display" );  	
-	$('.product_cat-online').removeClass( "no-display" );
+	$('.product_cat-online').removeClass( "no-display" );  	
   }
 });
 $( "#campus-courses").click(function() {
   $( this ).addClass( "active" ).siblings().removeClass('active'); 
+  	$('.offline').addClass( "active" );	 
+  	$('.online').removeClass( "active" );  	  
   if (!$( this ).hasClass( "active" )) {
   	$('.product_cat-on-campus').removeClass( "display" );
-	$('.product_cat-online').removeClass( "no-display" );  	
+	$('.product_cat-online').removeClass( "no-display" );
   } else {
 	$('.product_cat-on-campus').addClass( "display" );
-	$('.product_cat-online').addClass( "no-display" );	
+	$('.product_cat-online').addClass( "no-display" );		
   }
 });  
 </script>
--- woocommerce.php
+++ woocommerce.php
@@ -35,11 +35,21 @@ get_header(); ?>
 
 	<!-- Text Header -->	
 
-	<article class="online-on-campus">
+	<article class="online-on-campus text-center">
 		<header>
-			<h2><?php the_field('awesome_skills_title'); ?>Online Courses Heading</h2>
-			<h4><?php the_field('awesome_skills_description'); ?></h4>
-		</header>
+			<div class="hidden">
+				<h2><?php the_field('awesome_skills_title'); ?>Online Courses Heading</h2>
+				<h4><?php the_field('awesome_skills_description'); ?>Online Courses Subheading</h4>
+			</div>
+			<div class="online active">
+				<h2><?php the_field('awesome_skills_title'); ?>Online Courses Heading</h2>
+				<h4><?php the_field('awesome_skills_description'); ?>Online Courses Subheading</h4>
+			</div>
+			<div class="offline">
+				<h2><?php the_field('awesome_skills_title'); ?>Offline Courses Heading</h2>
+				<h4><?php the_field('awesome_skills_description'); ?>Offline Courses Subheading</h4>
+			</div>	
+		</header>		
 	</article>
 
 	<!-- // Text Header -->	
